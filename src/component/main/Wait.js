57ac0324df2d9c7bb963ba3ce24bc3c96e303da3
DESKTOP-QGIVFM8\user <dbsek21@naver.com>
2022-06-22 22:33:06 +0900

    add 슬라이더,

--- src/component/main/Wait.js
+++ src/component/main/Wait.js
@@ -1,16 +1,22 @@
 
 import Carousel from 'react-material-ui-carousel'
 
-const Wait = () => {
+import image1 from '../../images/1.jpg';
+import image2 from '../../images/2.jpg';
+import image3 from '../../images/3.jpg';
 
+const Wait = () => {
 
     return(
         <>
-            <Carousel autoPlay animation="slide">
-                {
-                    path.map( (item) => <Item item={item} /> )
-                }
-            </Carousel>
+            <div className="slider">
+              <Carousel autoPlay animation="slide">
+                  <img className="slider-image" src={image1} />
+                   <img className="slider-image" src={image2} />
+                   <img className="slider-image" src={image3} />
+             </Carousel>
+                <p className="wait-text">원하시는 언어를 터치해주세요.</p>
+            </div>
         </>
     );
 };
